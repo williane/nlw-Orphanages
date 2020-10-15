fd46ce880e874df3373fce27c721157a42e627b9
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2020-10-15 17:35:20 -0300

    nlw - aula 4

---
+++ src/database/fakedata.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-27.222633",
+        lng:  "-49.6455874",
+        name: "Lar das Meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1602647675036-52cffb901015?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
+            "https://images.unsplash.com/photo-1594269146507-03861ba52e8d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "1"
+
+    },
+    {
+        id: 2,
+        lat: "-27.222633",
+        lng:  "-49.6555874",
+        name: "Lar dos Meninos",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1602647675036-52cffb901015?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
+            "https://images.unsplash.com/photo-1594269146507-03861ba52e8d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 08h até 18h",
+        open_on_weekends: "0"
+
+    }
+]
